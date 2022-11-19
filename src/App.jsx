14e9c7a925068da7d4b9a3fe9446325fb873eff5
DCastro Coder <jonathandelcas@gmail.com>
2022-11-19 13:26:41 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles/App.scss";
+
+function App() {
+  return (
+    <>
+      <div className="card card-container">
+        <h1>Insta-ya</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
